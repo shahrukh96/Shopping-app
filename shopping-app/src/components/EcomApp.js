15748975a005a032bcfdacbfd6e1841f299f9535
Shahrukh Karim <shahrukhkarim466@gmail.com>
2019-01-22 19:30:51 +0530

    commit with home

--- shopping-app/src/components/EcomApp.js
+++ shopping-app/src/components/EcomApp.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
-import Navbar from './Navbar';
+import React, { Component } from "react";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Category from "./Category";
+import { Switch, Route } from "react-router";
 
 class EcomApp extends Component {
-    render() {
-        return (
-            <div>  
-                <Navbar />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/category" component={Category} />
+        </Switch>
+      </div>
+    );
+  }
 }
 export default EcomApp;
--- shopping-app/src/index.js
+++ shopping-app/src/index.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
+import EcomApp from "./components/EcomApp";
 
-
-
-import * as serviceWorker from './serviceWorker';
-import EcomApp from './components/EcomApp';
-
-ReactDOM.render(<EcomApp />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <BrowserRouter>
+    <EcomApp />
+  </BrowserRouter>,
+  document.getElementById("root")
+);

60ece9f9404288d73b5fa920d12927e64182c570
Mojahid <hussainmojahid3@gmail.com>
2019-01-23 14:59:17 +0530

    Reducer Created(Mojahid)

---
+++ shopping-app/src/reducers/CategoryReducer.js
@@ -0,0 +1,13 @@
+import * as c from '../constants/CategoryConstant';
+
+var insitialState={
+    categories:[
+        {id:1345,name:'Electronics',description:"Item Related to Electronics"},
+        {id:5468,name:'Grocery',description:"Item Related Daily Home Needs"}
+    ]
+};
+
+export default function categoryReducer(state=insitialState,action){
+
+    return state;
+}
